4e49a692fb4e0c06e6058035342be10574990612
harvies <harvies@localhost.localdomain>
2017-07-19 12:21:48 +0800

    1

--- src/test/java/com/harvies/demo/elasticsearch/App.java
+++ src/test/java/com/harvies/demo/elasticsearch/App.java
@@ -6,11 +6,17 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Date;
-
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetItemResponse;
+import org.elasticsearch.action.get.MultiGetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
@@ -54,7 +60,7 @@ public class App {
 
 	@Test
 	public void get() {
-		GetResponse response = client.prepareGet("index", "type2", "1").get();
+		GetResponse response = client.prepareGet("twitter", "tweet", "1").get();
 		System.out.println(response);
 	}
 
@@ -100,7 +106,7 @@ public class App {
 		updateRequest.type("tweet");
 		updateRequest.id("1");
 		try {
-			updateRequest.doc(jsonBuilder().startObject().field("message111", "hello111").endObject());
+			updateRequest.doc(jsonBuilder().startObject().field("message", "2017年莫斯科国际航空航天展览会").endObject());
 			UpdateResponse updateResponse = client.update(updateRequest).get();
 			System.out.println(updateResponse.toString());
 		} catch (Exception e) {
@@ -136,6 +142,55 @@ public class App {
 			e.printStackTrace();
 		}
 
+	}
+
+	@Test
+	public void multiGet() {
+		MultiGetResponse multiGetItemResponses = client.prepareMultiGet().add("twitter", "tweet", "1")
+				.add("twitter", "tweet", "2", "3", "4").add("another", "type", "foo").get();
+
+		for (MultiGetItemResponse itemResponse : multiGetItemResponses) {
+			GetResponse response = itemResponse.getResponse();
+			if (response.isExists()) {
+				String json = response.getSourceAsString();
+				System.out.println(json);
+			}
+		}
+	}
+
+	@Test
+	public void bulk() {
+		BulkRequestBuilder bulkRequest = client.prepareBulk();
+
+		// either use client#prepare, or use Requests# to directly build
+		// index/delete requests
+		try {
+			bulkRequest.add(client.prepareIndex("twitter", "tweet", "1").setSource(jsonBuilder().startObject().field("user", "kimchy")
+					.field("postDate", new Date()).field("message", "trying out Elasticsearch").endObject()));
+
+			bulkRequest.add(client.prepareIndex("twitter", "tweet", "2").setSource(jsonBuilder().startObject().field("user", "kimchy")
+					.field("postDate", new Date()).field("message", "another post").endObject()));
+
+			BulkResponse bulkResponse = bulkRequest.get();
+			for (BulkItemResponse bulkItemResponse : bulkResponse) {
+				System.out.println(bulkItemResponse.getResponse());
+			}
+			if (bulkResponse.hasFailures()) {
+				// process failures by iterating through each bulk response item
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void search() {
+		SearchResponse response = client.prepareSearch("index", "twitter").setTypes("tweet", "type2")
+				.setSearchType(SearchType.DFS_QUERY_THEN_FETCH).setQuery(QueryBuilders.termQuery("message", "莫")) // Query
+				.setPostFilter(QueryBuilders.rangeQuery("age").from(12).to(24)) // Filter
+				.setFrom(0).setSize(60).setExplain(true).get();
+		System.out.println(response);
 	}
 
 	@After
